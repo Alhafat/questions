a1c56b27d905f2c33ace18319fd9fc68ae142214
Alhafat <vladimir-lukin@inbox.ru>
2023-01-17 16:35:43 +0300

    task_1

---
+++ exp_1/Program.cs
@@ -0,0 +1,96 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+
+// while (true) 
+// { 
+//     Console.Write("Введите первое число: "); 
+//     int a=int.Parse(Console.ReadLine()!); 
+ 
+//     Console.Write("Введите второе число: "); 
+//     int b=int.Parse(Console.ReadLine()!); 
+//         if (a==b) 
+//         { 
+//             Console.WriteLine($"Введи неравные числа!!!"); 
+//             continue; // Оператор пропускает текущую итерацию и переходит к следующей. 
+//         } 
+//         else 
+//         { 
+//             Console.Write("Введите третье число: "); 
+//             int c=int.Parse(Console.ReadLine()!); 
+//             if (b==c) 
+//             { 
+//                 Console.WriteLine($"Введи неравные числа!!!"); 
+//                 continue; // Оператор пропускает текущую итерацию и переходит к следующей. 
+//             }
+//         } 
+         
+             
+//                 if (a==c) 
+//                 { 
+//                     Console.WriteLine($"Введи неравные числа!!!"); 
+//                     continue; // Оператор пропускает текущую итерацию и переходит к следующей. 
+//                 }
+//                 else
+//                 { 
+//                     if (a>b) 
+//                     { 
+//                         if (a>c) 
+//                         { 
+//                             Console.WriteLine($"Максимальное число {a}");  
+//                             break; 
+//                         } 
+//                         else 
+//                         { 
+//                             if (b>c) 
+//                             { 
+//                                 Console.WriteLine($"Максимальное число {b}"); 
+//                                 break; 
+//                             } 
+//                             else 
+//                             { 
+//                                 Console.WriteLine($"Максимальное число {c}");  
+//                                 break; 
+//                             } 
+//                         } 
+//                     } 
+//                 } 
+//             } 
+        
+    
+// // Тут не место для break... в Вашем контексте 
+// // break; 
+while (true)
+Console.Write("Введите первое целое число"); 
+   int firstInt = int.Parse(Console.ReadLine()!); 
+   Console.Write("Введите второе целое число"); 
+   int secondInt = int.Parse(Console.ReadLine()!); 
+   // Alghafat: "если неравные давать третье для ввода" 
+   if (firstInt != secondInt) 
+   { 
+    Console.Write("Введите второе целое число"); 
+    int thirdInt = int.Parse(Console.ReadLine()!); 
+     
+    if (thirdInt != firstInt && thirdInt != secondInt) 
+    { 
+     int result = firstInt; 
+     if (result < secondInt) result = secondInt; 
+     if (result < thirdInt) result = thirdInt; 
+     Console.WriteLine($"Из введенных чисел: {firstInt}, {secondInt}, {thirdInt}; наибольшее  значение числа – {result}"); 
+    break; // Момотов Кирилл: "Результат получили, цикл прерываем" 
+    } 
+    else 
+    { 
+    Console.WriteLine("Первое число и второе число не должны быть равны!"); 
+    continue; // Момотов Кирилл: "Введены некорректные данные, идем на следующую итерацию" 
+    } 
+   } 
+   else 
+   { 
+    Console.WriteLine("Первое число и второе число не должны быть равны!"); 
+    continue; // Момотов Кирилл: "Введены некорректные данные, идем на следующую итерацию" 
+   } 
