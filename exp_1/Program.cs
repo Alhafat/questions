e46f0a62c03557968646ce3a4d39b6548b3eb1c9
Alhafat <vladimir-lukin@inbox.ru>
2023-01-17 17:00:24 +0300

    решение найдено

--- exp_1/Program.cs
+++ exp_1/Program.cs
@@ -6,91 +6,85 @@
 
 
 
-// while (true) 
-// { 
-//     Console.Write("Введите первое число: "); 
-//     int a=int.Parse(Console.ReadLine()!); 
+while (true) 
+{ 
+    Console.Write("Введите первое число: "); 
+    int a=int.Parse(Console.ReadLine()!); 
  
-//     Console.Write("Введите второе число: "); 
-//     int b=int.Parse(Console.ReadLine()!); 
-//         if (a==b) 
-//         { 
-//             Console.WriteLine($"Введи неравные числа!!!"); 
-//             continue; // Оператор пропускает текущую итерацию и переходит к следующей. 
-//         } 
-//         else 
-//         { 
-//             Console.Write("Введите третье число: "); 
-//             int c=int.Parse(Console.ReadLine()!); 
-//             if (b==c) 
-//             { 
-//                 Console.WriteLine($"Введи неравные числа!!!"); 
-//                 continue; // Оператор пропускает текущую итерацию и переходит к следующей. 
-//             }
-//         } 
-         
-             
-//                 if (a==c) 
-//                 { 
-//                     Console.WriteLine($"Введи неравные числа!!!"); 
-//                     continue; // Оператор пропускает текущую итерацию и переходит к следующей. 
-//                 }
-//                 else
-//                 { 
-//                     if (a>b) 
-//                     { 
-//                         if (a>c) 
-//                         { 
-//                             Console.WriteLine($"Максимальное число {a}");  
-//                             break; 
-//                         } 
-//                         else 
-//                         { 
-//                             if (b>c) 
-//                             { 
-//                                 Console.WriteLine($"Максимальное число {b}"); 
-//                                 break; 
-//                             } 
-//                             else 
-//                             { 
-//                                 Console.WriteLine($"Максимальное число {c}");  
-//                                 break; 
-//                             } 
-//                         } 
-//                     } 
-//                 } 
-//             } 
-        
-    
-// // Тут не место для break... в Вашем контексте 
-// // break; 
-while (true)
-Console.Write("Введите первое целое число"); 
-   int firstInt = int.Parse(Console.ReadLine()!); 
-   Console.Write("Введите второе целое число"); 
-   int secondInt = int.Parse(Console.ReadLine()!); 
-   // Alghafat: "если неравные давать третье для ввода" 
-   if (firstInt != secondInt) 
-   { 
-    Console.Write("Введите второе целое число"); 
-    int thirdInt = int.Parse(Console.ReadLine()!); 
+    Console.Write("Введите второе число: "); 
+    int b=int.Parse(Console.ReadLine()!); 
+        if (a==b) 
+        { 
+            Console.WriteLine($"Введите неравные числа!!!"); 
+            continue; // Оператор возвращает в начало цикла. 
+        } 
+        else 
+        { 
+            Console.Write("Введите третье число: "); 
+            int c=int.Parse(Console.ReadLine()!); 
+            if ((c==a)^(c==b)) 
+            { 
+                Console.WriteLine($"Введите неравные числа!!!"); 
+                continue; // Оператор возвращает в начало цикла.
+            }
+            else
+            {
+                if ((a>b)&(a>c))
+                {
+                    Console.WriteLine($"Максимальное число {a}"); 
+                    break; 
+                }
+                else
+                {
+                    if (b>c)
+                    { 
+                        Console.WriteLine($"Максимальное число {b}"); 
+                        break; 
+                    } 
+                    else
+                    {
+                        Console.WriteLine($"Максимальное число {c}");
+                        break;
+                    }
+
+                }
+
+            }
+        } 
+}    
+// Тут не место для break... в Вашем контексте 
+// break; 
+
+// эта же задача чуть по другому, но работает!!!
+// while (true)
+// {
+// Console.Write("Введите первое целое число: "); 
+//    int firstInt = int.Parse(Console.ReadLine()!); 
+//    Console.Write("Введите второе целое число: "); 
+//    int secondInt = int.Parse(Console.ReadLine()!); 
+//    // Alghafat: "если неравные давать третье для ввода" 
+//    if (firstInt != secondInt) 
+//    { 
+//     Console.Write("Введите второе целое число: "); 
+//     int thirdInt = int.Parse(Console.ReadLine()!); 
      
-    if (thirdInt != firstInt && thirdInt != secondInt) 
-    { 
-     int result = firstInt; 
-     if (result < secondInt) result = secondInt; 
-     if (result < thirdInt) result = thirdInt; 
-     Console.WriteLine($"Из введенных чисел: {firstInt}, {secondInt}, {thirdInt}; наибольшее  значение числа – {result}"); 
-    break; // Момотов Кирилл: "Результат получили, цикл прерываем" 
-    } 
-    else 
-    { 
-    Console.WriteLine("Первое число и второе число не должны быть равны!"); 
-    continue; // Момотов Кирилл: "Введены некорректные данные, идем на следующую итерацию" 
-    } 
-   } 
-   else 
-   { 
-    Console.WriteLine("Первое число и второе число не должны быть равны!"); 
-    continue; // Момотов Кирилл: "Введены некорректные данные, идем на следующую итерацию" 
-   } 
+//     if (thirdInt != firstInt && thirdInt != secondInt) 
+//     { 
+//      int result = firstInt; 
+//      if (result < secondInt) result = secondInt; 
+//      if (result < thirdInt) result = thirdInt; 
+//      Console.WriteLine($"Из введенных чисел: {firstInt}, {secondInt}, {thirdInt}; наибольшее  значение числа – {result}"); 
+//     break; // Момотов Кирилл: "Результат получили, цикл прерываем" 
+//     } 
+//     else 
+//     { 
+//     Console.WriteLine("Первое число и второе число не должны быть равны!"); 
+//     continue; // Момотов Кирилл: "Введены некорректные данные, идем на следующую итерацию" 
+//     } 
+//    } 
+//    else 
+//    { 
+//     Console.WriteLine("Первое число и второе число не должны быть равны!"); 
+//     continue; // Момотов Кирилл: "Введены некорректные данные, идем на следующую итерацию" 
+//    } 
+// }
