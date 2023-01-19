f663d42bc6683606ce1164a0a18a432cb54cdd3c
Alhafat <vladimir-lukin@inbox.ru>
2023-01-19 22:58:51 +0300

    exp_2

---
+++ exp_2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+bool errorInput = true;
+int inputNumber = 1;
+while (errorInput)
+{
+    Console.Write("Введите трёхзначное число (100-999): ");
+    inputNumber = int.Parse(Console.ReadLine());
+    if (inputNumber < 100 || 999 < inputNumber)
+    {
+        errorInput = true;
+        Console.Write("Ошибка ввода. ");
+        continue;
+    }
+    else
+        errorInput = false;
+}
+//                                   abc число. Искомое - b  = >   abc / 10 = ab    ab % 10 = b 
+Console.Write($" Вы ввели число {inputNumber}. Искомая 2-a цифра: {(inputNumber / 10) % 10}");
