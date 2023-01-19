07021ac7f6b43a88cb569de09a865a927ac8b47a
Alhafat <vladimir-lukin@inbox.ru>
2023-01-19 23:22:30 +0300

    exp_3

---
+++ exp_03/Program.cs
@@ -0,0 +1,32 @@
+﻿///123456 ///3
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
+int i = int.Parse(Console.ReadLine());
+
+int count=0;
+int result=0;
+
+if (number>=100)
+{
+    while (count<i)
+    {
+        // if (count<i+1)                            
+        // {
+        result=number/10;
+        count++;
+        // }
+        // else
+        // {
+        //     Console.Write(result);
+        //     break;
+        // }
+    }
+    Console.Write(result);
+            // break;
+}
+else
+{
+    Console.WriteLine("no");
+}
